40a1d9e77831676f458132c1e65f60f85828502c
CTFD Admin <ethanshang@berkeley.edu>
2025-01-06 08:16:59 -0800

    Configure site and base path in Astro config

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'calm-the-fi-down.github.io',
+    base: '/Calm-the-Fi-Down/'
+});
